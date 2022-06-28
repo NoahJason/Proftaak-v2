2f4205af0c65640b8ce7b0cfd2d8ac62604c8b3c
NoahJason <noahjasontaiapin@gmail.com>
2022-06-28 11:39:21 +0200

    Added game
    Games have been set in place with css

--- Site/game.php
+++ Site/game.php
@@ -4,15 +4,27 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/stylesnavbar.css">
-    <link rel="stylesheet" href="css/stylesabout.css">
+    <link rel="stylesheet" href="css/stylesgame.css">
     <script defer src="js/slides.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet" />
 </head>
 
-<body id="aboutpg">
+<body id="gamepg">
     <?php include 'navbar.php'?>
     <main>
-        
+    <h1>Game page</h1>
+    <p>This page is for the games. </br>
+        All games are connected thru a URL so all games dont stack over eachoter. </br>
+        All games are online and can be downloaded, all games are also selfmade.</p>
+        <div class="run">
+            <a href="https://noahtai.itch.io/my-game" class="game">Run game</a>
+        </div>
+        <div class="snake">
+        <a href="url" class="Snake">Snake</a>
+        </div>
+        <div class="pong">
+        <a href="url" class="Pong">Pong</a>
+        </div>
     </main>
     <?php include 'footer.php'?>
 </body>
